7191bcaf54ec740f29131bcc5b7669c2d9313c53
Emerson Amaro <emersona@csdomingos.local.sp>
2023-04-24 13:11:03 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,7 @@
+const texto1 = "Hello World";
+const texto2 = " Hello World 2";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = " alguem disse oi !";
+console.log(citacao)
