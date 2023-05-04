34555d28a82318e22a7c31c63dc428969729860d
Emerson Amaro <emersona@csdomingos.local.sp>
2023-05-04 16:51:22 -0300

    first commit

---
+++ arrow-function.js
@@ -0,0 +1,19 @@
+function apresentar(nome){
+    return`meu nome é ${nome}`;
+    }
+
+
+    const apresentaArrow = nome => `meu nome é ${nome}`;
+    const soma = (num1, num2) => num1 + num2 ;
+
+    // Arrow fuction com + de uma linha de instrução
+
+    const somaNumerosPequenos = (num1, num2) => {
+        if (num1 > 10 || num2 > 10){
+            return"somente numeros dee 1 a 9"
+        }else{
+            return num1 + num2
+        }
+    }
+    
+    
